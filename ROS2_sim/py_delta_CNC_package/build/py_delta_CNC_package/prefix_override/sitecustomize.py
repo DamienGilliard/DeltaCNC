cc53b22c8e9a45ecf50ad2da2f3b137ebe25cf8a
DamienGilliard <damien.gilliard@epfl.ch>
2025-07-25 00:43:20 +0200

    feat: add structure of py_delta_CNC_package folder

--- ROS2_sim/py_delta_CNC_package/build/py_delta_CNC_package/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/damien/DeltaCNC/ROS2_sim/py_delta_CNC_package/install/py_delta_CNC_package'
